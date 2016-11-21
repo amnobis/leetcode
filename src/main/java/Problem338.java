6cfff2a4374f7fee2aa3a7623bc36ff97a4965a2
anobis <anobis@purdue.edu>
2016-11-20 23:14:09 -0600

    Shorten solution

--- src/main/java/Problem338.java
+++ src/main/java/Problem338.java
@@ -11,27 +11,8 @@ public class Problem338 {
     public static int[] countBits(int num) {
         int[] bits = new int[num + 1];
 
-        if (num == 0) {
-            return new int[]{0};
-        } else if (num == 1) {
-            return new int[]{0, 1};
-        } else if (num == 2) {
-            return new int[]{0, 1, 1};
-        }
-
-        bits[0] = 0;
-        bits[1] = 1;
-        bits[2] = 1;
-
-        for (int i = 3; i <= num; ++i) {
-            if ((i & (i - 1)) == 0) {
-                bits[i] = 1;
-            } else {
-                int val = (int) (Math.log(i) / Math.log(2));
-                int val2 = i - (int) Math.pow(2, val);
-
-                bits[i] = 1 + bits[val2];
-            }
+        for (int i = 1; i <= num; ++i) {
+            bits[i] = bits[i >> 1] + (i & 1);
         }
 
         return bits;
